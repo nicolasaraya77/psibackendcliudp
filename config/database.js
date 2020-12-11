534a115a39f97e8fc8042693518dc48f9be3bff3
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-12-11 14:12:50 -0300

    fix database credentials

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "../variables.env" });
 module.exports = {
   // Configuracion de DB
   username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || null,
+  password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_DATABASE || "psicliudp",
   host: process.env.DB_HOST || "127.0.0.1",
   dialect: process.env.DB_DIALECT || "mysql",
